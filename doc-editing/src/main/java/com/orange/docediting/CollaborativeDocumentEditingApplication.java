2602a6b9569d80ad90fe2c673d682f1d1c7ef031
Anjali <anjali.gaikwad18@vit.edu>
2023-12-10 14:23:29 +0530

    Initial Spring Boot Setup

---
+++ doc-editing/src/main/java/com/orange/docediting/CollaborativeDocumentEditingApplication.java
@@ -0,0 +1,13 @@
+package com.orange.docediting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeDocumentEditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeDocumentEditingApplication.class, args);
+	}
+
+}
---
+++ doc-editing/src/test/java/com/orange/docediting/CollaborativeDocumentEditingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.docediting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeDocumentEditingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
